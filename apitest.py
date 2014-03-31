fdd077a4dec31d872e3ceae68ee62101b505b8fa
Marcus Kempe <makem86@gmail.com>
2014-03-31 11:47:58 +0200

    Added further experimental methods for getting data from the api.

--- apitest.py
+++ apitest.py
@@ -2,18 +2,39 @@ import base64
 import urllib2
 import json
 
+
+
 def main():
-    url = "http://api.tagtider.net/v1/stations.json"
+    #print getStations()
+    getDepartures(getIdFromName('Hovmantorp'))
+
+def auth(url):
     passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
     passman.add_password(None, url, "tagtider", "codemocracy")
     opener = urllib2.build_opener(urllib2.HTTPBasicAuthHandler(passman),
                          urllib2.HTTPDigestAuthHandler(passman))
-
     urllib2.install_opener(opener)
     pagehandle = urllib2.urlopen(url)
-    
+    return pagehandle
+
+def getStations():
+    pagehandle = auth("http://api.tagtider.net/v1/stations.json")    
     stations = json.load(pagehandle)
-    for station in stations['stations']['station']:
-        print station['name']
+    return stations['stations']['station']
+
+def getIdFromName(name):
+    for stations in getStations():
+        if(stations['name'] == name):
+            return stations['id']
+    return -1
+
+def getDepartures(stationId):    
+    if stationId > -1:
+        url = "http://api.tagtider.net/v1/stations/%s/transfers/departures.json" %(stationId)
+        pagehandle = auth(url)    
+        deps = json.load(pagehandle)
+        print deps
+        for transfer in deps['station']['transfers']['transfer']:
+            print transfer['destination'],' ',transfer['departure'],' Track:',transfer['track']
 
 if  __name__ =='__main__':main()
